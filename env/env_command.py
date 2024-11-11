0313f9e51bae8de93167174f191784efecb69dc7
liumengyuan <1052635979@qq.com>
2024-11-11 17:22:58 +0800

    在env_command中增加了记录车的函数,但因为屏蔽掉初始化车位置固定,env_base中仍然有bug,有的车避让完停住不动.

--- env/env_command.py
+++ env/env_command.py
@@ -1,3 +1,4 @@
+import copy
 import json
 from components.base_env import RefinementEnv
 from env.components.vehicle import Action
@@ -10,10 +11,9 @@ class EnvRecord(RefinementEnv):
 
         self.temp_vehicles = {}
         self.temp_stations = {}
+        self.init_vehicles_pos = {}
 
         self.data = {}
-        self.tracks = {}
-        self.time = []
 
     def output_to_file(self, path):
         with open(path, 'w', encoding='utf-8') as file:
@@ -32,6 +32,7 @@ class EnvRecord(RefinementEnv):
             result[f"{vehicle.name}_INIT_LOAD_STATE"] = 1
             result[f"{vehicle.name}_INIT_LOAD_STATE_REMARK"] = "空载-1"
             result[f"{vehicle.name}_INIT_POSITION"] = init_pos
+            self.init_vehicles_pos[vehicle.name] = init_pos
 
         for station in self.stations.values():
             if station.type == 'workstation':
@@ -54,23 +55,20 @@ class EnvRecord(RefinementEnv):
         return result
 
     def record_vehicle(self):
+        cur_time = self.sys_time
+
         for vehicle in self.vehicles.values():
             if vehicle.type == 'crane':
                 cur_pos = [vehicle.pos, vehicle.other_dim_pos]
-                if vehicle.load_degree != 0:
-                    tar_pos = [vehicle]
-                else:
-                    tar_pos = []
             elif vehicle.type == 'trolley':
                 cur_pos = [vehicle.other_dim_pos, vehicle.pos]
             else:
                 raise ValueError(f"{vehicle.type} vehicle type not supported")
             cur_action = vehicle.action
-            cur_time =
-            cur_load_state =
-            cur_load_state_remark =
-            cur_state =
-            cur_remark =
+            cur_load_state = vehicle.load_degree
+            # cur_load_state_remark =
+            # cur_state =
+            # cur_remark =
 
             # 车第一次接受指令时init
             if self.temp_vehicles[vehicle.name] is None:
@@ -78,25 +76,87 @@ class EnvRecord(RefinementEnv):
                     continue
                 else:
                     # 写入新动作
-                    self.temp_vehicles[vehicle.name] = {'pre_action': cur_action,
-                                                        'begin_pos': cur_pos,
+                    self.temp_vehicles[vehicle.name] = {'action': cur_action,
+                                                        'begin_pos': self.init_vehicles_pos[vehicle.name],
+                                                        'tar_pos': cur_pos,
+                                                        'start_time': cur_time,
+                                                        'end_time': cur_time,
+                                                        'load_state': cur_load_state,
+                                                        'type': vehicle.type,
+                                                        'name': vehicle.name,
+                                                        'is_operating': vehicle.is_operating
                                                         }
             else:
-                if cur_action != self.temp_vehicles[vehicle.name]['pre_action']:
+                if (cur_action != self.temp_vehicles[vehicle.name]['action'] or
+                        vehicle.is_operating != self.temp_vehicles[vehicle.name]['is_operating']):
                     # TODO 把之前的记录压入data中,然后写入新动作
+                    self.temp_vehicles[vehicle.name]['end_time'] = cur_time
+                    self.translate_result(copy.deepcopy(self.temp_vehicles[vehicle.name]))
+
+                    self.temp_vehicles[vehicle.name]['begin_pos'] = self.temp_vehicles[vehicle.name]['tar_pos']
+                    self.temp_vehicles[vehicle.name]['pre_action'] = cur_action
+                    self.temp_vehicles[vehicle.name]['start_time'] = self.temp_vehicles[vehicle.name]['end_time']
+                    self.temp_vehicles[vehicle.name]['tar_pos'] = cur_pos
+                    self.temp_vehicles[vehicle.name]['load_state'] = cur_load_state
+                    self.temp_vehicles[vehicle.name]['is_operating'] = vehicle.is_operating
+
                 else:
                     # 动作相同, 更新记录
+                    self.temp_vehicles[vehicle.name]['tar_pos'] = cur_pos
+                    self.temp_vehicles[vehicle.name]['end_time'] = cur_time
 
+
+
+    def translate_result(self, result):
+        load_degree_remark = {0: '空载-1', 1: '满载-2', 2: '重载-3'}
+        if result['load_state'] == 0:
+            if result['is_operating']:
+                state = 2
+            else:
+                if result['action'] == Action.STAY:
+                    state = 5
+                else:
+                    state = 1
+        elif result['load_state'] == 1:
+            raise ValueError(f"{result['load_state']} load state not supported")
+        elif result['load_state'] == 2:
+            if result['is_operating']:
+                state = 4
+            else:
+                if result['action'] == Action.STAY:
+                    state = 5
+                else:
+                    state = 3
+        else:
+            raise ValueError(f"{result['load_state']} load state not supported")
+        crane_remark = {1: '空移-1', 2: '起吊-2', 3: '载重移动-3', 4: '落吊-4', 5: '等待-5'}
+        trolley_remark = {1: '空移-1', 2: '装载-2', 3: '载重移动-3', 4: '落吊-4', 5: '等待-5'}
+        bc_type = '天车' if result['type'] == 'crane' else '台车'
+        remark = crane_remark[state] if bc_type == '天车' else trolley_remark[state]
+
+        translated_result = {'BC_ID': result['name'],
+                             'BC_TYPE': bc_type,
+                             'BEGIN_POSITION': result['begin_pos'],
+                             'TARGET_POSITION': result['tar_pos'],
+                             'START_TIME': result['start_time'],
+                             'END_TIME': result['end_time'],
+                             'LOAD_STATE': result['load_state'],
+                             'LOAD_STATE_REMARK': load_degree_remark[result['load_state']],
+                             'STATE': state,
+                             'REMARK': remark
+                             }
+
+        self.data['BRIDGECRANE_PLAN'].append(translated_result)
     def record_station(self):
         pass
 
     def record_all(self):
-        data = {
-            "INITIALINFORMATION": self.record_reset,
+        self.data = {
+            "INITIALINFORMATION": self.record_reset(),
             "BRIDGECRANE_PLAN": [],
             "EQUIPMENT_STATE": []
         }
-
+        print(self.temp_vehicles)
         running = True
         while running:
             print('*' * 40 + f' {self.sys_time} ' + '*' * 40)
@@ -113,6 +173,8 @@ class EnvRecord(RefinementEnv):
 if __name__ == '__main__':
     recorder = EnvRecord(config_path='config/refine_env_record.yaml',
                          task_file_path='data/processed_data/processed_data.json')
-    init_information = recorder.record_reset()
-    recorder.data = {"INITIALINFORMATION": init_information}
+    recorder.record_all()
     recorder.output_to_file('data/command/GlobalInformation.json')
+    # init_information = recorder.record_reset()
+    # recorder.data = {"INITIALINFORMATION": init_information}
+    # recorder.output_to_file('data/command/GlobalInformation.json')
